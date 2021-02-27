31a2fb865a282319f859d388dc3a473833f1f296
Sergey Reshetnikov <shaman@simmirra.com>
2021-02-27 07:21:38 +0300

    Change depth of reexport

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,16 @@
-pub mod rfs;
+//! This library provides simple util for testing file system operations.
+//! When you test something you need some sandbox which should wipe out after testing finish.
+//! This package allow you configure temporary directory and its inner structure, perform tests and
+//! remove it when all work will done.
+
+mod rfs;
+
+pub use rfs::error::FsTesterError;
+
+pub use rfs::fs_tester::Config;
+pub use rfs::fs_tester::ConfigEntry;
+pub use rfs::fs_tester::DirectoryConf;
+pub use rfs::fs_tester::FileConf;
+pub use rfs::fs_tester::FileContent;
+pub use rfs::fs_tester::FsTester;
+
--- src/rfs/mod.rs
+++ src/rfs/mod.rs
@@ -1,13 +1,2 @@
 pub mod error;
-
 pub mod fs_tester;
-
-// reexport
-pub use error::FsTesterError;
-
-pub use fs_tester::Config;
-pub use fs_tester::ConfigEntry;
-pub use fs_tester::DirectoryConf;
-pub use fs_tester::FileConf;
-pub use fs_tester::FileContent;
-pub use fs_tester::FsTester;
