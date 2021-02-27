1cdadd63d316dec0a532eb2d836423710114a53e
Sergey Reshetnikov <shaman@simmirra.com>
2021-02-27 11:59:10 +0300

    Remove from reexport inner structs

--- src/lib.rs
+++ src/lib.rs
@@ -3,14 +3,9 @@
 //! This package allow you configure temporary directory and its inner structure, perform tests and
 //! remove it when all work will done.
 
-mod rfs;
+pub mod rfs;
 
 pub use rfs::error::FsTesterError;
 
-pub use rfs::fs_tester::Config;
-pub use rfs::fs_tester::ConfigEntry;
-pub use rfs::fs_tester::DirectoryConf;
-pub use rfs::fs_tester::FileConf;
-pub use rfs::fs_tester::FileContent;
 pub use rfs::fs_tester::FsTester;
 
