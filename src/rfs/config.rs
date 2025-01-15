750c1b09744eeae41841a7456353ca18dedde0a0
Sergey Reshetnikov <shaman@simmirra.com>
2025-01-16 01:09:04 +0300

    Added structs reexport from config mod

--- src/rfs/config.rs
+++ src/rfs/config.rs
@@ -1,4 +1,11 @@
 pub mod config_entry;
+pub mod configuration;
 pub mod directory_conf;
 pub mod file_conf;
 pub mod link_conf;
+
+pub use config_entry::ConfigEntry;
+pub use configuration::Configuration;
+pub use directory_conf::DirectoryConf;
+pub use file_conf::FileConf;
+pub use link_conf::LinkConf;
