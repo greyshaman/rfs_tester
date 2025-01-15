59af312e9389aeb73bb6ddfd431cb476e5daaee3
Sergey Reshetnikov <shaman@simmirra.com>
2025-01-16 01:19:57 +0300

    fixed code style

--- src/rfs/fs_tester_error.rs
+++ src/rfs/fs_tester_error.rs
@@ -4,46 +4,38 @@ use std::fmt;
 /// Configuration parse error
 #[derive(Debug)]
 pub enum FsTesterError {
-  EmptyConfig,
-  BaseDirNotFound,
-  ShouldFromDirectory,
-  // ParseYaml,
-  // Io(io::Error),
-  // ParseJson(serde_json::Error),
+    EmptyConfig,
+    BaseDirNotFound,
+    ShouldFromDirectory,
+    // ParseYaml,
+    // Io(io::Error),
+    // ParseJson(serde_json::Error),
 }
 
 impl fmt::Display for FsTesterError {
-  /// fmt implementation for FsTesterError
-  /// handle different cases
-  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-
-    match *self {
-      FsTesterError::EmptyConfig => write!(f, "Config should not be empty"),
-      FsTesterError::BaseDirNotFound =>
-        write!(
-          f,
-          "Base directory not found!"
-        ),
-      FsTesterError::ShouldFromDirectory =>
-        write!(
-          f,
-          "Config should start from containing directory"
-        ),
-      // FsTesterError::ParseYaml => write!(f, "Config is not satisfy the yaml format"),
-      // FsTesterError::ParseJson(ref e) => e.fmt(f),
-      // FsTesterError::Io(ref e) => e.fmt(f),
+    /// fmt implementation for FsTesterError
+    /// handle different cases
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            FsTesterError::EmptyConfig => write!(f, "Config should not be empty"),
+            FsTesterError::BaseDirNotFound => write!(f, "Base directory not found!"),
+            FsTesterError::ShouldFromDirectory => {
+                write!(f, "Config should start from containing directory")
+            } // FsTesterError::ParseYaml => write!(f, "Config is not satisfy the yaml format"),
+              // FsTesterError::ParseJson(ref e) => e.fmt(f),
+              // FsTesterError::Io(ref e) => e.fmt(f),
+        }
     }
-  }
 }
 
 impl std::error::Error for FsTesterError {
-  // fn source(&self) -> Option<&(dyn error::Error + 'static)> {
-  //   match *self {
-  //     FsTesterError::EmptyConfig | FsTesterError::ParseYaml => None,
-  //     FsTesterError::ParseJson(ref e) => Some(e),
-  //     FsTesterError::Io(ref e) => Some(e),
-  //   }
-  // }
+    // fn source(&self) -> Option<&(dyn error::Error + 'static)> {
+    //   match *self {
+    //     FsTesterError::EmptyConfig | FsTesterError::ParseYaml => None,
+    //     FsTesterError::ParseJson(ref e) => Some(e),
+    //     FsTesterError::Io(ref e) => Some(e),
+    //   }
+    // }
 }
 
 // impl From<serde_json::Error> for FsTesterError {
