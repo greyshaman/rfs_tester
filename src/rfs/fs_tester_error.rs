bf2836b13a53851131ef65c41f28f810afec036b
Sergey Reshetnikov <shaman@simmirra.com>
2025-02-27 19:18:45 +0300

    Added test units for FsTesterError

--- src/rfs/fs_tester_error.rs
+++ src/rfs/fs_tester_error.rs
@@ -48,11 +48,6 @@ impl FsTesterError {
         fs_tester_error!(ErrorCode::ShouldStartFromDirectory)
     }
 
-    /// Construct error instance in case when function gets unexpected type of ConfEntry
-    pub fn unexpected_conf_entry_type() -> Self {
-        fs_tester_error!(ErrorCode::UnexpectedConfEntryType)
-    }
-
     /// If any non-allowed settings are found in the configuration, an error instance will be created.
     pub fn not_allowed_settings() -> Self {
         fs_tester_error!(ErrorCode::LinksNotAllowed)
@@ -61,6 +56,11 @@ impl FsTesterError {
     /// An error instance is created when an input/output error occurs.
     pub fn io_error(err: std::io::Error) -> Self {
         fs_tester_error!(ErrorCode::Io(err))
+    }
+
+    /// An error instance is created when an walkdir error occurs.
+    pub fn walkdir_error(err: walkdir::Error) -> Self {
+        fs_tester_error!(ErrorCode::WalkDir(err))
     }
 
     /// One-based line at which the error was detected.
@@ -91,9 +91,7 @@ impl FsTesterError {
     /// - `Category::Io` - failure to read or write data
     pub fn classify(&self) -> Category {
         match self.err.code {
-            ErrorCode::EmptyConfig
-            | ErrorCode::ShouldStartFromDirectory
-            | ErrorCode::UnexpectedConfEntryType => Category::ConfigFormat,
+            ErrorCode::EmptyConfig | ErrorCode::ShouldStartFromDirectory => Category::ConfigFormat,
             ErrorCode::LinksNotAllowed => Category::NotAllowedSettings,
             ErrorCode::JsonSyntax(_) | ErrorCode::YamlSyntax(_) => Category::Syntax,
             ErrorCode::Io(_) | ErrorCode::WalkDir(_) => Category::Io,
@@ -173,10 +171,6 @@ pub(crate) enum ErrorCode {
     /// has links entries notify this error
     LinksNotAllowed,
 
-    /// In the build directory, we expect a Directory or Clone Directory type,
-    /// and if we receive any other type, we should throw an Error.
-    UnexpectedConfEntryType,
-
     /// Yaml parser encountered error.
     YamlSyntax(serde_yaml::Error),
 
@@ -226,12 +220,6 @@ impl Display for ErrorCode {
                     "#
                 )
             }
-            ErrorCode::UnexpectedConfEntryType => {
-                write!(
-                    f,
-                    "An unexpected config entry type occurred in configuration."
-                )
-            }
             ErrorCode::WalkDir(err) => write!(f, "Walkdir error: {}", err),
             ErrorCode::Io(err) => write!(f, "IO error: {}", err),
             ErrorCode::JsonSyntax(err) => write!(f, "JSON syntax error: {}", err),
@@ -246,7 +234,7 @@ impl Display for FsTesterError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         Display::fmt(&*self.err, f)?;
         if let Some(sandbox_dir) = &self.sandbox_dir {
-            write!(f, " Created dir {} will be removed.", sandbox_dir,)?;
+            write!(f, " Created dir \"{}\" will be removed.", sandbox_dir,)?;
         }
 
         Ok(())
@@ -283,7 +271,12 @@ impl std::error::Error for FsTesterError {
             ErrorCode::Io(err) => Some(err),
             ErrorCode::JsonSyntax(err) => Some(err),
             ErrorCode::YamlSyntax(err) => Some(err),
-            _ => None,
+            ErrorCode::WalkDir(err) => Some(err),
+            ErrorCode::AcquireError(err) => Some(err),
+            ErrorCode::JoinError(err) => Some(err),
+            ErrorCode::EmptyConfig
+            | ErrorCode::LinksNotAllowed
+            | ErrorCode::ShouldStartFromDirectory => None,
         }
     }
 }
@@ -351,7 +344,10 @@ impl From<JoinError> for FsTesterError {
 
 #[cfg(test)]
 mod tests {
+    use std::error::Error;
+
     use tokio::sync::Semaphore;
+    use walkdir::WalkDir;
 
     use super::*;
 
@@ -377,6 +373,18 @@ mod tests {
         let error = FsTesterError::io_error(io_error);
         assert!(error.is_io());
         assert_eq!(error.io_error_kind(), Some(std::io::ErrorKind::NotFound));
+    }
+
+    #[test]
+    fn test_io_error_kind_return_none() {
+        let error = FsTesterError::empty_config();
+        assert!(error.io_error_kind().is_none());
+    }
+
+    #[test]
+    fn test_is_empty_config() {
+        let error = FsTesterError::empty_config();
+        assert!(error.is_empty_config());
     }
 
     #[test]
@@ -432,6 +440,247 @@ mod tests {
         assert!(error.is_syntax());
     }
 
+    #[test]
+    fn test_display_fmt_for_empty_config() {
+        let error = FsTesterError::empty_config();
+
+        assert_eq!(
+            format!("{}", error),
+            "The configuration should not be empty."
+        );
+    }
+
+    #[test]
+    fn test_display_fmt_for_should_start_from_directory() {
+        let error = FsTesterError::should_start_from_directory();
+
+        assert_eq!(
+            format!("{}", error),
+            "The configuration should start from the containing directory."
+        );
+    }
+
+    #[test]
+    fn test_display_fmt_for_links_not_allowed() {
+        let error = FsTesterError::not_allowed_settings();
+
+        assert_eq!(
+            format!("{}", error),
+            r#"
+                    The use of links has been disabled!
+                    !!! Be warned that the contents of linked files may be corrupted !!!
+                    If you want to enable the use of links, you can do so at your own risk
+                    by setting the LINKS_ALLOWED environment variable to "Y".
+                    "#
+        );
+    }
+
+    #[test]
+    fn test_display_fmt_for_walkdir_error() {
+        let walkdir_error = WalkDir::new("./blahblah")
+            .into_iter()
+            .next()
+            .expect("should return first entry?")
+            .expect_err("should be error on non-existent directory");
+
+        let error = FsTesterError::walkdir_error(walkdir_error);
+
+        assert_eq!(format!("{}", error), "Walkdir error: IO error for operation on ./blahblah: No such file or directory (os error 2)");
+    }
+
+    #[test]
+    fn test_display_fmt_for_io_error() {
+        let io_error = std::fs::File::open("blah.blah")
+            .expect_err("should return error for attempt to open of non-existent file");
+        let error = FsTesterError::io_error(io_error);
+
+        assert_eq!(
+            format!("{}", error),
+            "IO error: No such file or directory (os error 2)"
+        );
+    }
+
+    #[test]
+    fn test_display_fmt_for_json_syntax_error() {
+        let json_data = r#"{ "number": "not_a_number" }"#;
+        let json_error = serde_json::from_str::<i32>(json_data).unwrap_err();
+
+        let error = FsTesterError::from(json_error);
+
+        assert_eq!(format!("{}", error), "JSON syntax error: invalid type: map, expected i32 at line 1 column 0 at line 1 column 0")
+    }
+
+    #[test]
+    fn test_display_fmt_for_yaml_syntax_error() {
+        let invalid_yaml = "invalid: yaml: [";
+        let yaml_error = serde_yaml::from_str::<serde_yaml::Value>(invalid_yaml).unwrap_err();
+
+        let error = FsTesterError::from(yaml_error);
+
+        assert_eq!(format!("{}", error), "YAML syntax error: mapping values are not allowed in this context at line 1 column 14 at line 1 column 14");
+    }
+
+    #[test]
+    fn test_display_fmt_with_sandbox_dir() {
+        let mut error = FsTesterError::empty_config();
+        error.set_sandbox_dir(Some("sandbox_dir".to_string()));
+
+        assert!(format!("{}", error).contains("Created dir \"sandbox_dir\" will be removed."));
+    }
+
+    #[test]
+    fn test_debug_fmt_implementation() {
+        let invalid_yaml = "invalid: yaml: [";
+        let yaml_error = serde_yaml::from_str::<serde_yaml::Value>(invalid_yaml).unwrap_err();
+
+        let error = FsTesterError::from(yaml_error);
+        assert_eq!(
+            format!("{:?}", error),
+            "FsTesterError {YamlSyntax(Error { kind: SCANNER, problem: \"mapping values are not allowed in this context\", problem_mark: Mark { line: 1, column: 14 } }), line: 1, column: 14, message: YAML syntax error: mapping values are not allowed in this context at line 1 column 14 at line 1 column 14 }",
+        );
+    }
+
+    #[test]
+    fn test_error_source_for_io_error() {
+        let io_error = std::fs::File::open("blah.blah")
+            .expect_err("should return error for attempt to open of non-existent file");
+        let error = FsTesterError::io_error(io_error);
+
+        assert_eq!(
+            format!("{:?}", error.source()),
+            "Some(Os { code: 2, kind: NotFound, message: \"No such file or directory\" })"
+        );
+    }
+
+    #[test]
+    fn test_error_source_for_json_syntax_error() {
+        let json_data = r#"{ "number": "not_a_number" }"#;
+        let json_error = serde_json::from_str::<i32>(json_data).unwrap_err();
+
+        let error = FsTesterError::from(json_error);
+
+        assert_eq!(
+            format!("{:?}", error.source()),
+            "Some(Error(\"invalid type: map, expected i32\", line: 1, column: 0))",
+        );
+    }
+
+    #[test]
+    fn test_error_source_for_yaml_syntax_error() {
+        let invalid_yaml = "invalid: yaml: [";
+        let yaml_error = serde_yaml::from_str::<serde_yaml::Value>(invalid_yaml).unwrap_err();
+
+        let error = FsTesterError::from(yaml_error);
+
+        assert_eq!(
+            format!("{:?}", error.source()),
+            "Some(Error { kind: SCANNER, problem: \"mapping values are not allowed in this context\", problem_mark: Mark { line: 1, column: 14 } })",
+        );
+    }
+
+    #[test]
+    fn test_error_source_for_walkdir_error() {
+        let walkdir_error = WalkDir::new("./blahblah")
+            .into_iter()
+            .next()
+            .expect("should return first entry?")
+            .expect_err("should be error on non-existent directory");
+
+        let error = FsTesterError::walkdir_error(walkdir_error);
+
+        assert_eq!(
+            format!("{:?}", error.source()),
+            "Some(Error { depth: 0, inner: Io { path: Some(\"./blahblah\"), err: Os { code: 2, kind: NotFound, message: \"No such file or directory\" } } })",
+        );
+    }
+
+    #[test]
+    fn test_error_source_for_empty_config_error() {
+        let error = FsTesterError::empty_config();
+
+        assert_eq!(format!("{:?}", error.source()), "None");
+    }
+
+    #[test]
+    fn test_error_source_for_links_not_allowed_error() {
+        let error = FsTesterError::not_allowed_settings();
+
+        assert_eq!(format!("{:?}", error.source()), "None");
+    }
+
+    #[test]
+    fn test_error_source_for_should_start_from_directory_error() {
+        let error = FsTesterError::should_start_from_directory();
+
+        assert_eq!(format!("{:?}", error.source()), "None");
+    }
+
+    #[test]
+    fn test_error_from_for_walkdir_error() {
+        let walkdir_error = WalkDir::new("./blahblah")
+            .into_iter()
+            .next()
+            .expect("should return first entry?")
+            .expect_err("should be error on non-existent directory");
+
+        let error = FsTesterError::from(walkdir_error);
+
+        assert!(error.is_io());
+    }
+
+    #[test]
+    fn test_from_fs_tester_error_for_io_error() {
+        let io_error = std::fs::File::open("blah.blah").expect_err("should be error");
+        let error = FsTesterError::io_error(io_error);
+        let converted_io_error = std::io::Error::from(error);
+
+        assert!(converted_io_error.kind() == ErrorKind::NotFound);
+    }
+
+    #[test]
+    fn test_from_fs_tester_error_for_io_error_invalid_data() {
+        let error = FsTesterError::empty_config();
+        let converted_io_error = std::io::Error::from(error);
+
+        assert!(converted_io_error.kind() == ErrorKind::InvalidData);
+    }
+
+    #[test]
+    fn test_from_io_error_into_fs_tester_error() {
+        let io_error = std::fs::File::open("blah.blah").expect_err("should be error");
+        let error = FsTesterError::from(io_error);
+
+        assert!(error.is_io());
+    }
+
+    #[tokio::test]
+    async fn test_display_fmt_for_acquire_error() {
+        let semaphore = Semaphore::const_new(2);
+        semaphore.close();
+        let acquire_error = semaphore
+            .acquire()
+            .await
+            .expect_err("closed semaphore should return return error");
+        let error = FsTesterError::from(acquire_error);
+
+        assert_eq!(format!("{}", error), "Semaphore err: semaphore closed");
+    }
+
+    #[tokio::test]
+    async fn test_display_fmt_for_join_error() {
+        let handle = tokio::spawn(async move {
+            panic!("test JoinError");
+        });
+
+        let join_error = handle
+            .await
+            .expect_err("panic in handle should produce JoinError");
+        let error = FsTesterError::from(join_error);
+
+        assert!(format!("{}", error).contains("Join handle err: task"));
+        assert!(format!("{}", error).contains("panicked with message \"test JoinError\""));
+    }
+
     #[tokio::test]
     async fn test_acquire_error() {
         let semaphore = Semaphore::new(2);
@@ -457,5 +706,32 @@ mod tests {
         let error = FsTesterError::from(join_error);
 
         assert!(error.is_multitasking());
+    }
+
+    #[tokio::test]
+    async fn test_error_source_for_acquire_error() {
+        let semaphore = Semaphore::new(2);
+        semaphore.close();
+        let acquire_error = semaphore
+            .acquire()
+            .await
+            .expect_err("closed semaphore should return error");
+        let error = FsTesterError::from(acquire_error);
+
+        assert_eq!(format!("{:?}", error.source()), "Some(AcquireError(()))");
+    }
+
+    #[tokio::test]
+    async fn test_error_source_for_join_error() {
+        let handle = tokio::spawn(async move {
+            panic!("test JoinError");
+        });
+
+        let join_error = handle
+            .await
+            .expect_err("panic in handle should produce JoinError");
+        let error = FsTesterError::from(join_error);
+
+        assert!(format!("{:?}", error.source()).contains("JoinError::Panic"));
     }
 }
